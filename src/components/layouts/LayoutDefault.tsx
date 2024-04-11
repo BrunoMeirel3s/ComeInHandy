18788f473a2659129478566ca2ec58d142b502c7
Bruno Meireles <brunomeireles23@gmail.com>
2024-04-10 22:47:16 -0300

    wip: ajustes na tela inicial

--- src/components/layouts/LayoutDefault.tsx
+++ src/components/layouts/LayoutDefault.tsx
@@ -1,10 +1,13 @@
 import Image from "next/image";
 import { SignIn } from "@phosphor-icons/react";
+import { Modal } from "antd";
+import { useState } from "react";
 
 export default function LayoutDefault({ children }) {
+  const [modalOpen, setModalOpen] = useState<boolean>(false);
   return (
     <>
-      <div className="px-4 flex flex-row items-center justify-between">
+      <div className="px-4 flex flex-row items-center justify-between p-4">
         <Image
           src={"/fulllogo.png"}
           alt="ComeInHandy"
@@ -12,13 +15,28 @@ export default function LayoutDefault({ children }) {
           height={51}
         />
 
-        <div className="flex flex-row">
+        <button
+          className="flex flex-row items-center justify-center space-x-2"
+          onClick={() => setModalOpen(!modalOpen)}
+        >
           <span className="text-gray-200">Fazer Login</span>{" "}
           <span>
-            <SignIn size={20} style={{ color: "#50B2C0" }} />
+            <SignIn size={20} color={"#50B2C0"} />
           </span>
-        </div>
+        </button>
       </div>
+      <Modal
+        style={{}}
+        centered
+        open={modalOpen}
+        onOk={() => setModalOpen(false)}
+        onCancel={() => setModalOpen(false)}
+        footer={null}
+      >
+       <div className="mt-8">
+          <p className="text-center text-xl">Faça login para utilizar as funcionalidades</p>
+       </div>
+      </Modal>
       <div className="z-auto overflow-y-auto w-full min-h-full p-8">
         {children}
       </div>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,22 +1,66 @@
 import Head from "next/head";
-import Image from "next/image";
-import { Inter } from "@next/font/google";
+import { Password } from "@phosphor-icons/react";
 
 export default function Home() {
   return (
     <>
       <Head>
         <title>Come In Handy</title>
-        <meta name="description" content="Conjunto de ferramentas para o dia a dia" />
+        <meta
+          name="description"
+          content="Conjunto de ferramentas para o dia a dia"
+        />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={""}>
-        <div>
-          <span className="text-2xl text-purple-200">Lorena princesa do papai</span>
+        <div className="flex flex-row items-center justify-around mt-8">
+          <div>
+            <img src="it_support.svg" alt="Suporte de TI" />
+          </div>
+          <div className="w-1/3">
+            <p className="text-3xl text-gray-100 text-center text-bold">
+              Nosso site possui um conjunto de ferramentas gratuitas para
+              facilitar tarefas do dia a dia, experimente!
+            </p>
+          </div>
         </div>
-        <div>
-          <span className="text-2xl text-gray-200">Jair principe do papai</span>
+        <div className="flex flex-col mt-8">
+          <h1 className="text-2xl text-bold">Funcionalidades</h1>
+          <div className="flex flex-row justify-center mt-8 gap-4 md:gap-8 wrap">
+            <div className="flex flex-row h-14 w-80 rounded-lg overflow-hidden">
+              <div className="bg-purple-200 w-1/4 flex items-center justify-center">
+                <Password size={32} style={{ background: "transparent" }} />
+              </div>
+              <div className="bg-gray-700 text-xl w-3/4 flex items-center justify-center">
+                Gerador de Senhas
+              </div>
+            </div>
+            <div className="flex flex-row h-14 w-80 rounded-lg overflow-hidden">
+              <div className="bg-purple-200 w-1/4 flex items-center justify-center">
+                <Password size={32} style={{ background: "transparent" }} />
+              </div>
+              <div className="bg-gray-700 text-xl w-3/4 flex items-center justify-center">
+                Gerador de Senhas
+              </div>
+            </div>
+            <div className="flex flex-row h-14 w-80 rounded-lg overflow-hidden">
+              <div className="bg-purple-200 w-1/4 flex items-center justify-center">
+                <Password size={32} style={{ background: "transparent" }} />
+              </div>
+              <div className="bg-gray-700 text-xl w-3/4 flex items-center justify-center">
+                Gerador de Senhas
+              </div>
+            </div>
+            <div className="flex flex-row h-14 w-80 rounded-lg overflow-hidden">
+              <div className="bg-purple-200 w-1/4 flex items-center justify-center">
+                <Password size={32} style={{ background: "transparent" }} />
+              </div>
+              <div className="bg-gray-700 text-xl w-3/4 flex items-center justify-center">
+                Gerador de Senhas
+              </div>
+            </div>
+          </div>
         </div>
       </main>
     </>
